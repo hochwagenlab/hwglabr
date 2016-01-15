a60202d0a7191d16f09602cc1518c89aa0ebc971
luisvalesilva <luisvalesilva@gmail.com>
2016-01-15 15:12:30 -0500

    Added progress bar argument

--- R/readall_tab.R
+++ R/readall_tab.R
@@ -2,35 +2,59 @@
 #'
 #' This function allows you to load tab-separated wiggle data.
 #' Adapted from function written by Tovah Markowitz (original function name: 'readAll.tab').
-#' @param fileLocation The path to the folder containing the wiggle data. No default.
+#' @param fileLocation A string with the path to the folder containing the wiggle data. No default.
 #' @param use_readr Boolean indicating whether to use the much faster 'read_tsv' from
 #' Hadley Wickham's 'readr' package instead of base R's 'read.table'. Defaults to FALSE.
+#' @param progress_bar Boolean indicating whether to display a progress bar (using R package
+#'  'pbapply'). Defaults to FALSE.
 #' @return An R list of 16 data frames, one for each chromosome.
 #' @examples
 #' readall_tab("/Path/to/wiggles/folder/Red1_WT_reps_SacCer3_2mis_MACS_wiggle_norm/")
+#' #' readall_tab("/Path/to/wiggles/", use_readr = TRUE, progress_bar = TRUE)
 #' @export
 
-readall_tab <- function(fileLocation, use_readr = FALSE) {
+readall_tab <- function(fileLocation, use_readr = FALSE, progress_bar = FALSE) {
   ptm <- proc.time()
   filenames <- list.files(fileLocation, full = T)
-  # check if 'all chromosomes' file exists
+  # Check if 'all chromosomes' file exists and skipt it
   if (length(filenames) == 17) {
     filenames <- filenames[2:17]
   }
   
   if (use_readr) {
-    # Open the required package
+    # Check for package
     if (!requireNamespace("readr", quietly = TRUE)) {
-      stop("R package 'readr' needed for this function to work. Please install it:\n",
+      stop("R package 'readr' required. Please install it:\n",
            "install.packages('readr')", call. = FALSE)
     }
-    library(readr)    
-    alldata <- lapply(filenames, read_tsv, skip = 2, col_names = FALSE, progress = FALSE)
+    library(readr)
+    if (progress_bar) {
+      # Check for package
+      if (!requireNamespace("pbapply", quietly = TRUE)) {
+        stop("R package 'pbapply' required. Please install it:\n",
+             "install.packages('pbapply')", call. = FALSE)
+      }
+      library(pbapply)
+      alldata <- pblapply(filenames, read_tsv, skip = 2, col_names = FALSE, progress = FALSE)
+    } else {
+      alldata <- lapply(filenames, read_tsv, skip = 2, col_names = FALSE, progress = FALSE)
+    }
+    
   } else {
-    alldata <- lapply(filenames, read.table, skip = 2, sep = "\t") 
+    if (progress_bar) {
+      # Check for package
+      if (!requireNamespace("pbapply", quietly = TRUE)) {
+        stop("R package 'pbapply' required. Please install it:\n",
+             "install.packages('pbapply')", call. = FALSE)
+      }
+      library(pbapply)
+      alldata <- pblapply(filenames, read.table, skip = 2, sep = "\t")
+    } else {
+      alldata <- lapply(filenames, read.table, skip = 2, sep = "\t")
+    }
   }
   names(alldata) = filenames
-    
+  
   # Check reference genome
   check_S288C <- any(grep('chrI.', names(alldata), fixed = TRUE))
   check_SK1 <- any(grep('chr01.', names(alldata), fixed = TRUE))
