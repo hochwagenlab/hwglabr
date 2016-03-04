b2e3edb6b86484f29035b3692afb243c617d2dff
luisvalesilva <luisvalesilva@gmail.com>
2016-03-04 14:26:51 -0500

    Changes by Tovah: grep not including 'chr'

--- R/signal_at_summit.R
+++ R/signal_at_summit.R
@@ -3,42 +3,41 @@
 #' This function allows you to pull out the ChIP signal centered around summits or 
 #' midpoints of bedfiles if start and stop values are more than one base apart. It
 #' takes as input wiggle data as a list of 16 chromosomes (output of \code{readall_tab()})
-#' and a bedfile determining the positions to extract. Use with \code{signal_average()}
+#' and a bedfile determining the positions to extract. Use with \code{signal_average} 
 #' to calculate the mean around all midpoints.
-#' Function written by Tovah Markowitz.
 #' @param inputData A list of 16 chr wiggle data (output of readall_tab). No default.
 #' @param bedData A data frame of bed data to extract signal around. No default.
 #' @param extension Number indicating the number of bases around the summit or midpoints.
 #' Value will be added both upstream and downstream. Default is 1000 bp.
-#' @param onlyComplete Many wiggle files are missing data at random positions across the
+#' @param only_complete Many wiggle files are missing data at random positions across the
 #' genome. Boolean indicates if you want to only keep regions where every base is 
 #' included within the wiggle file. Default is \code{TRUE}.
-#' @return An R (dplyr) data frame with four columms: chromosome name, position (relative 
-#' to midpoint), signal, and bed line number within its individual chromosome.
+#' @return An R (deplyr) data frame with four columms: chromosome name, position (relative 
+#' to midpoint), signal, and bed line number within its individual chromosome
 #' @examples
 #' summit_plot(WT,red1_summit_bed)
 #'
-#' summit_plot(WT, S288Ccen, extension=2e4, onlyComplete = FALSE)
+#' summit_plot(WT, S288Ccen, extension=2e4, only_complete=FALSE)
 #' @export
 
 # library(hwglabr)
 # read in wiggle file using readall_tab
+
+# 3/3/16 function has been updated to allow bedfiles that do not have data for every chromosome
 
 #bedData <- read.table("BedFiles/AH6408I-042415-SacCer3-2mis-M2-Tagnorm_P15_summits.bed",stringsAsFactors=F)
 #bedData_factor <- read.table("BedFiles/AH6408I-042415-SacCer3-2mis-M2-Tagnorm_P15_summits.bed")
 
 #inputData <- Red1WT_A
 
-# want to include ability to plot around midpoints of bed_file as well before giving to Luis
-# also gff?
-
-signal_at_summit <- function(inputData, bedData, extension = 1000, onlyComplete = TRUE) {
+signal_at_summit <- function(inputData, bedData, extension = 1000, only_complete = TRUE) {
 ptm <- proc.time()
 
 if (!requireNamespace("dplyr", quietly = TRUE)) {
         stop("R package 'dplyr' needed for this function to work. Please install it.\n", 
             "install.packages('dplyr')", call. = FALSE)
     }
+    library(dplyr)
     
 # Check kind of numerals used in your input data (list of chromosome wiggle data)
 check_S288C <- any(grep('chrI.', names(inputData), fixed = TRUE))
@@ -59,7 +58,7 @@ if ( length( levels(bedData[,1]) ) != 0 ) {
 }
 
 # ensure bedData is a bed file
-if (grepl("chr", bedData[1,1]) & is.numeric(bedData[1,2]) & is.numeric(bedData[1,3])) {
+if (grepl("chr",bedData[1,1]) & is.numeric(bedData[1,2]) & is.numeric(bedData[1,3])) {
 	# ensure bedData is a summits file, or turn it into one
 	if (sum( (bedData[,3]- bedData[,2]) != 1) != 0) {
 		for (i in 1:nrow(bedData)) {
@@ -72,12 +71,12 @@ if (grepl("chr", bedData[1,1]) & is.numeric(bedData[1,2]) & is.numeric(bedData[1
 # ensure bedData uses the same reference genome as wiggle file
 if (check_S288C) {
 	cat ("Wiggle data is S288C.\n")
-	if ( !( any(bedData[,1] == "chrI") ) ) {
+	if ( !( any( grepl("I",bedData[,1]) ) ) ) {
 		stop ("bedData and wiggle_data do not use the same reference genome.")
 	} else {  chrom_nums <- chrom_S288C }
 } else if (check_SK1) {
 	cat ("Wiggle data is SK1.\n")
-	if ( !( any(bedData[,1] == "chr01") ) ) {
+	if ( !( any(grepl("0",bedData[,1])) ) ) {
 		stop ("bedData and wiggle_data do not use the same reference genome.")
 	} else { chrom_nums <- chrom_SK1 }
 } else stop('Did not recognize reference genome.')
@@ -85,7 +84,7 @@ if (check_S288C) {
 # ensure bedData is a bed file
 if (grepl("chr",bedData[1,1]) & is.numeric(bedData[1,2]) & is.numeric(bedData[1,3])) {
 	# ensure bedData is a summits file, or turn it into one
-	if (sum( (bedData[,3]- bedData[,2]) != 1) != 0) {2
+	if (sum( (bedData[,3]- bedData[,2]) != 1) != 0) {
 		# determine regions of genome to extract wiggle plot information
 		size <- nrow (bedData)
 		summit_range <- data.frame (chr=character(size), summit=numeric(size), start=numeric(size), 
@@ -127,47 +126,52 @@ for (i in 1:length(chrom_nums)) {
 	# get wiggle information and rows from bedData associated with particular chromosome
 	list_index <- grep(paste0(chrom_nums[i], '.'), names(inputData), fixed = TRUE)
 	wiggle_data_chrom <- inputData[[list_index]]
-	summit_range_chrom <- summit_range[summit_range[, 'chr'] == chrom_nums[i], ]
-	cat (paste0(chrom_nums[i], ":\t", nrow(summit_range_chrom), " summits,\t"))
-	count = 0
-	for (j in 1:nrow(summit_range_chrom)) {
-		# find start position or closest thing
-		if ( length( which( wiggle_data_chrom[,1] >= summit_range_chrom[j,3]) )  == 0) {
-			a <- 1
-		} else {
-			a <- min( which( wiggle_data_chrom[,1] >= summit_range_chrom[j,3]) )
-		}
+	# to allow for some chromosomes to not have values in bedData
+	if ( !( any(summit_range[, 'chr'] == chrom_nums[i])) ) {
+		next
+	} else {
+		summit_range_chrom <- summit_range[summit_range[, 'chr'] == chrom_nums[i], ]
+		cat (paste0(chrom_nums[i], ":\t", nrow(summit_range_chrom), " summits,\t"))
+		count = 0
+		for (j in 1:nrow(summit_range_chrom)) {
+			# find start position or closest thing
+			if ( length( which( wiggle_data_chrom[,1] >= summit_range_chrom[j,3]) )  == 0) {
+				a <- 1
+			} else {
+				a <- min( which( wiggle_data_chrom[,1] >= summit_range_chrom[j,3]) )
+			}
 		
-		# find end position or closest thing
-		if ( length( which( wiggle_data_chrom[,1] <= summit_range_chrom[j,4]) ) == 0 ) {
-			b <- nrow(wiggle_data_chrom)
-		} else {
-			b <- max( which( wiggle_data_chrom[,1] <= summit_range_chrom[j,4]) )
-		}
-		if (onlyComplete) {
-			if ( (b-a) == (2 * extension) ) {
-				count <- count + 1
-				tmp <- wiggle_data_chrom[a:b,]
-				colnames(tmp) <- c("position","signal")
-				tmp[,1] <- tmp[,1] - summit_range_chrom[j,2]
-				tmp2 <- data.frame( chr=rep( chrom_nums[i], nrow(tmp) ), tmp, line=rep( j, nrow(tmp) ), 
-					stringsAsFactors=F)
-				summit_data <- dplyr::bind_rows(summit_data,tmp2)
+			# find end position or closest thing
+			if ( length( which( wiggle_data_chrom[,1] <= summit_range_chrom[j,4]) ) == 0 ) {
+				b <- nrow(wiggle_data_chrom)
+			} else {
+				b <- max( which( wiggle_data_chrom[,1] <= summit_range_chrom[j,4]) )
 			}
-		} else {
-			# make sure I didn't end up with the entire chromosome
-			if ( (a==1) & (b==nrow(wiggle_data_chrom)) ) {
-				next
-			} else if ( (a < b) & ((b-a) <= (2 * extension) ) ) { 
-				count <- count + 1
-				tmp <- wiggle_data_chrom[a:b,]
-				colnames(tmp) <- c("position","signal")
-				tmp[,1] <- tmp[,1] - summit_range_chrom[j,2]
-				tmp2 <- data.frame( chr=rep( chrom_nums[i], nrow(tmp) ), tmp, line=rep( j, nrow(tmp) ), 
-					stringsAsFactors=F)
-				summit_data <- dplyr::bind_rows(summit_data,tmp2)
-			}
+			if (only_complete) {
+				if ( (b-a) == (2 * extension) ) {
+					count <- count + 1
+					tmp <- wiggle_data_chrom[a:b,]
+					colnames(tmp) <- c("position","signal")
+					tmp[,1] <- tmp[,1] - summit_range_chrom[j,2]
+					tmp2 <- data.frame( chr=rep( chrom_nums[i], nrow(tmp) ), tmp, line=rep( j, nrow(tmp) ), 
+						stringsAsFactors=F)
+					summit_data <- bind_rows(summit_data,tmp2)
+				}
+			} else {
+				# make sure I didn't end up with the entire chromosome
+				if ( (a==1) & (b==nrow(wiggle_data_chrom)) ) {
+					next
+				} else if ( (a < b) & ((b-a) <= (2 * extension) ) ) { 
+					count <- count + 1
+					tmp <- wiggle_data_chrom[a:b,]
+					colnames(tmp) <- c("position","signal")
+					tmp[,1] <- tmp[,1] - summit_range_chrom[j,2]
+					tmp2 <- data.frame( chr=rep( chrom_nums[i], nrow(tmp) ), tmp, line=rep( j, nrow(tmp) ), 
+						stringsAsFactors=F)
+					summit_data <- bind_rows(summit_data,tmp2)
+				}
 		}
+	}
 	}
 	cat(paste0(count," summits mapped\n"))	
 }
