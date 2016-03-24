9373fa4ecd46701431bd0770497d2874b7a4d027
luisvalesilva <luisvalesilva@gmail.com>
2016-03-24 10:03:53 -0400

    Make loop depend on length of input list, rather than being ficex to 16

--- R/signal_at_orf.R
+++ R/signal_at_orf.R
@@ -51,8 +51,9 @@ signal_at_orf <- function(inputData, gff, gffFile, loessSpan = 0.05, saveFile = 
   }
   
   # Check reference genome for both the input data and the gff file; make sure they match
-  chrom_S288C <- c("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
-                   "XI", "XII", "XIII", "XIV", "XV", "XVI")
+  chrom_S288C <- c("chrI", "chrII", "chrIII", "chrIV", "chrV", "chrVI", "chrVII",
+                   "chrVIII", "chrIX", "chrX", "chrXI", "chrXII", "chrXIII", "chrXIV",
+                   "chrXV", "chrXVI")
   chrom_SK1 <- c('01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
                  '11', '12', '13', '14', '15', '16')
   
@@ -86,7 +87,7 @@ signal_at_orf <- function(inputData, gff, gffFile, loessSpan = 0.05, saveFile = 
   plus_final <- data.frame()
   minus_final <- data.frame()
   
-  for(i in 1:length(chrom)) {
+  for(i in 1:length(inputData)) {
     chrNum <- paste0('chr', chrom[i])
     cat(paste0(chrNum, ':\n'))
     
