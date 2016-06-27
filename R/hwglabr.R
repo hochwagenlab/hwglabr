f1ddf9bca6cbd68da73d70491a3065209aa17f0a
luisvalesilva <luisvalesilva@gmail.com>
2016-06-27 17:45:23 -0400

    Minor changes to documentation

---
+++ R/hwglabr.R
@@ -0,0 +1,12 @@
+#' hwglabr: A compilation of functions used frequently by Hochwagen lab members.
+#'
+#' The hwglabr package provides several functions commonly used for data analysis
+#' in the Howchwagen lab, with a focus on ChIP-seq data analysis.
+#' 
+#' Even though some plotting functions are included, these are rather brittle. They
+#' can be used as a first pass plotting or as starter code to be adapted to the
+#' users' needs.
+#' 
+#' @docType package
+#' @name hwglabr
+NULL
