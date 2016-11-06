fa6564393e99eea59236fbb6fafd71f2d945b52a
luisvalesilva <luisvalesilva@gmail.com>
2016-11-06 17:08:07 -0500

    Elapsed time print handling both sec and min-long execution

--- R/wiggle_compress.R
+++ R/wiggle_compress.R
@@ -70,7 +70,7 @@ If this is a merged wiggle file, ensure first column is called 'position' and th
 	data <- data[ order( data$position ), ]	
 	data2 <- Compress( data, window )
 	
-	elapsed_time <- proc.time()[3] - ptm[3])
+	elapsed_time <- proc.time()[3] - ptm[3]
 	if(elapsed_time < 60){
 	  message(paste0("Completed in ", round(elapsed_time, 1), " sec."))
 	}else{
